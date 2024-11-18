d766aaa1d52c299daa51f414d5e819bc2646c572
Dani <slimshady3753@protonmail.com>
2024-11-18 04:42:20 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('load',()=>{
+    const activeShare =document.getElementById('active-share');
+    const shareIcon = document.getElementById('share-icon');
+    shareIcon.addEventListener('click',()=>{
+    activeShare.style.display==='block';
+})
+console.log('hi')
+})
